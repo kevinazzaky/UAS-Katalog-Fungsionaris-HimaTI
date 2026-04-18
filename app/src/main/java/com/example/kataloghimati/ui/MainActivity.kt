10dfb4a2f33a14a7c9837bbb9b8f8d981a699864
zakypacartifa <kevinazzaky1@gmail.com>
2026-04-18 21:44:18 +0800

    feat: atur navigasi utama dan jadikan halaman login sebagai layar awal aplikasi

--- app/src/main/java/com/example/kataloghimati/ui/MainActivity.kt
+++ app/src/main/java/com/example/kataloghimati/ui/MainActivity.kt
@@ -1,21 +1,51 @@
 package com.example.kataloghimati.ui
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import com.example.kataloghimati.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 1. Menangkap data Nama yang dikirim dari Halaman Login (MILESTONE MINGGU 2)
+        val namaPengguna = intent.getStringExtra("USER_NAME")
+        if (namaPengguna != null) {
+            Toast.makeText(this, "Selamat datang, $namaPengguna!", Toast.LENGTH_SHORT).show()
         }
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // 2. Buka HomeFragment secara otomatis saat pertama kali masuk
+        if (savedInstanceState == null) {
+            bukaFragment(HomeFragment())
+        }
+
+        // 3. Logika saat tombol navigasi bawah diklik
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    bukaFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_search -> {
+                    bukaFragment(SearchFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    // Fungsi pintar untuk mengganti-ganti isi layar (Fragment)
+    private fun bukaFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
     }
 }
